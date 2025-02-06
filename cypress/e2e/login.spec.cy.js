fc2ae1686cc4f29ea5c94fb9e20a83c02a5aea7e
VlFarias <vladsonmfarias@outlook.com>
2025-02-06 14:37:32 -0300

    Adding usar data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,35 +1,48 @@
+import userData from '../fixtures/user-data.json' // importação do json
+
 describe('Orange HRM Tests', () => {
 
   const selectorsList = { //criando um objeto usando {} - Dentro do objeto precisa usar virgula para finalizar cada linha
     usernameField: "[name='username']", // criado esse ojeto para facilitar a manutenção do cod, assim não precisa ficar renomendo
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module", // Foi substituida por dashboradGrid (Houve problema com a busca pelo nome)
+    dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialAlert: "[role='alert']"
   }
 
+  /*
+  const userData = { //Obejto passado para o fixture
+    userSuccess:{
+      username: 'Admin',
+      password: 'admin123'
+    }, //precisa de virgula
+
+    userFail:{
+      username: 'Test',
+      password: 'teste123'
+    }
+  }*/
+
   it('Login - Success', () => { //usando obejto
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorsList.dashboardGrid)
   })
 
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Test')//Usuário incorreto
-    cy.get(selectorsList.passwordField).type('test')//Senha incorreto
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)//Usuário incorreto
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)//Senha incorreto
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)
 
   })
 
-  // Para eu executar o cypress, usar o Git bash e o comando (npx cypress open)
-
 /*
-
   it('Login - Success', () => { //adicionado SKIP (it.skip) para pular o primeiro teste
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') //Paguina de teste
     cy.get("[name='username']").type('Admin') // local de teste, colocado o .type
